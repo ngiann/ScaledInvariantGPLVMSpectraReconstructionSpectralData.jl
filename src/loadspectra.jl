2ee0d1531eb76f4753581f723e56dbef3810ed5a
Nikos Gianniotis <ngiann@gmail.com>
2025-05-21 16:04:59 +0200

    amend message

--- src/loadspectra.jl
+++ src/loadspectra.jl
@@ -3,7 +3,7 @@ function loadspectra()
     λ, f, σ = JLD2.load(joinpath(artifact"sdssrm_spectra_data", "spectra.jld2"),"λ","f","σ")
 
     @printf("This dataset has been compiled by Iliana Isabel Cortés Pérez.\n")
-    @printf("Return 1185 spectra.\n")
+    @printf("Return 1256 spectra.\n")
     @printf("1st returned argument is the common grid of wavelengths.\n")
     @printf("2nd returned argument is the matrix of fluxes.\n")
     @printf("3rd returned argument is the matrix of standard errors.\n")
